885b1f4f8c6aa2445334417b51b7dedcc9565ccc
Youri <youridibbet@gmail.com>
2022-11-17 19:43:41 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items. """
+    with open(filepath, 'r') as local_file:
+        todos_local = local_file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write a to-do item in the to-do items list """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
